1f2952f42b369bb89a25f2a8b2dd79aff76bf430
Mike Shenin <i@kotrik.ru>
2019-10-20 18:05:10 +0300

    offset

--- vendor/github.com/KotRikD/krapi/usersmostplayedbm.go
+++ vendor/github.com/KotRikD/krapi/usersmostplayedbm.go
@@ -15,7 +15,8 @@ RIGHT JOIN beatmaps
 ON beatmaps.beatmap_md5 = scores.beatmap_md5
 WHERE scores.userid = %s AND scores.play_mode = %s
 GROUP BY scores.beatmap_md5
-ORDER BY cbm5 DESC;
+ORDER BY cbm5 DESC
+LIMIT 5 OFFSET %s;
 `
 
 func UsersMostPlayedBM(md common.MethodData) common.CodeMessager {
@@ -24,8 +25,9 @@ func UsersMostPlayedBM(md common.MethodData) common.CodeMessager {
 
 	mode := md.Query("mode")
 	uid := md.Query("uid")
+	offset := md.Query("offset")
 
-	rows, err := md.DB.Query(fmt.Sprintf(baseBeatmapSelect2, uid, mode))
+	rows, err := md.DB.Query(fmt.Sprintf(baseBeatmapSelect2, uid, mode, offset))
 	if err != nil {
 		md.Err(err)
 		return common.SimpleResponse(500, "An error occurred. Trying again may work. If it doesn't, yell at this Kotorikku instance admin and tell them to fix the API.")
